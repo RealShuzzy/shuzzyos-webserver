f11dcdfb275953e6deea9503c61bdbda0e48a49d
Shuzzy <samirvonhansen2016@gmail.com>
2025-10-15 08:46:22 +0200

    updated home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-[4000px] bg-nosferatu-900 text-light">
+    <div className="flex flex-col min-h-[4000px] bg-nosferatu-900 text-light p-15">
 
-      <div className="w-full h-[300px] bg-dracula m-15">
+      <div className="w-full h-full bg-dracula rounded-xl">
         <p>Home</p>
       </div>
 
